760f0303a15785247ca02565eabb801cf85a12cf
sepulzera <hartung_spam@gmx.de>
2019-01-06 10:43:11 +0100

    Remove SvDomCapPoints setting, as the feature was remove (see #22)

--- src/game/variables.h
+++ src/game/variables.h
@@ -134,7 +134,6 @@ MACRO_CONFIG_INT(SvVoteKickBantime, sv_vote_kick_bantime, 5, 0, 1440, CFGFLAG_SA
 // domination
 MACRO_CONFIG_STR(SvDomCapTimes, sv_dom_captimes, 32, "5 8 11 14 17 20 23 27", CFGFLAG_SAVE|CFGFLAG_SERVER, "Captimes for dynamic capturing")
 MACRO_CONFIG_STR(SvDomUseSpots, sv_dom_use_spots, 32, "1 1 1 1 1", CFGFLAG_SAVE|CFGFLAG_SERVER, "En-/disables domination spots")
-MACRO_CONFIG_INT(SvDomCapPoints, sv_dom_cappoints, 5, 0, 100, CFGFLAG_SAVE|CFGFLAG_SERVER, "Number of points a player gets for capturing a domination spot")
 
 MACRO_CONFIG_INT(SvDomRespawnDelay, sv_dom_respawn_delay, 2, 0, 60, CFGFLAG_SAVE|CFGFLAG_SERVER, "How many seconds to wait before respawning")
 MACRO_CONFIG_STR(SvDomScorings, sv_dom_scorings, 64, "2.0 1.0 0.7 0.5 0.4", CFGFLAG_SAVE|CFGFLAG_SERVER, "Dompoints for dynamic capturing")
