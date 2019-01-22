cef58b924a275a69edd8c11bd5b733f3dda252c0
sepulzera <hartung_spam@gmx.de>
2019-01-22 08:37:03 +0100

    Fix strike caparea E not working

--- src/game/server/gamemodes/strike.cpp
+++ src/game/server/gamemodes/strike.cpp
@@ -87,7 +87,7 @@ void CGameControllerSTRIKE::Tick()
 
 bool CGameControllerSTRIKE::OnEntity(int Index, vec2 Pos)
 {
-	if (Index + ENTITY_OFFSET >= TILE_DOM_FLAG_A && Index + ENTITY_OFFSET < TILE_DOM_CAPAREA_E)
+	if (Index + ENTITY_OFFSET >= TILE_DOM_FLAG_A && Index + ENTITY_OFFSET <= TILE_DOM_CAPAREA_E)
 		return CGameControllerDOM::OnEntity(Index, Pos);
 
 	int Type = -1;
