a9bcd38e3f2d33f1d10e4722686979496f86544f
sepulzera <hartung_spam@gmx.de>
2019-01-29 08:28:05 +0100

    Cleanup includes

--- src/game/server/gamemodes/strike.cpp
+++ src/game/server/gamemodes/strike.cpp
@@ -1,9 +1,6 @@
 /* (c) Magnus Auvinen. See licence.txt in the root of the distribution for more information. */
 /* If you are missing that file, acquire a complete release at teeworlds.com.                */
 #include <base/tl/base.h>
-#include <math.h>
-#include <float.h>
-#include <stdio.h>
 
 #include <engine/shared/config.h>
 
