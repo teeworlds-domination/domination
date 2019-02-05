cf091db9c10e2b8551628c73d3527370bb1025ba
sepulzera <hartung_spam@gmx.de>
2019-02-05 21:26:58 +0100

    Remove teeworlds copyright information

--- src/game/server/entities/csdom_flag.cpp
+++ src/game/server/entities/csdom_flag.cpp
@@ -1,6 +1,3 @@
-/* (c) Magnus Auvinen. See licence.txt in the root of the distribution for more information. */
-/* If you are missing that file, acquire a complete release at teeworlds.com.                */
-
 #include <game/server/gamecontext.h>
 #include <game/server/player.h>
 
